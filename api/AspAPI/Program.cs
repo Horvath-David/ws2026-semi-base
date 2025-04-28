66c13736f15d23691a557f2d5c7d86076a69e9c4
competitor2 <competitor2@skills.lan>
2025-04-28 14:25:14 +0200

    update quantity

--- api/AspAPI/Program.cs
+++ api/AspAPI/Program.cs
@@ -210,4 +210,65 @@ app.MapPost("/api/carts/items", async (DbContext db, [FromBody] JsonElement? bod
     return Results.Ok(new CartModel(cart.Id.ToString(), cart.CreatedAt.ToString("s").Split(".").First(), completeItems.Sum(x => x.TotalPrice), [.. completeItems]));
 });
 
+app.MapPatch("/api/carts/items/{itemId}", async (DbContext db, string itemId, [FromBody] JsonElement? body, [FromHeader] string Authorization = "") => {
+    var hc = new HttpClient();
+
+    UserInfo? user;
+    try {
+        hc.DefaultRequestHeaders.Add("Authorization", Authorization);
+        user = await hc.GetFromJsonAsync<UserInfo?>("http://idp.skills.lan/api/authentication/info");
+    } catch {
+        return Results.Unauthorized();
+    }
+
+    if (!body.HasValue) return Results.BadRequest();
+
+    int? newQuantity = body.Value!.TryGetProperty("newQuantity", out var _newQuantityProperty)
+        ? _newQuantityProperty.ValueKind == JsonValueKind.Number ?
+            _newQuantityProperty.TryGetInt32(out var _newQuantity)
+                ? _newQuantity
+                : null : null
+        : null;
+
+    if (newQuantity == null) return Results.BadRequest();
+
+    var cart = db.Carts.FirstOrDefault(x => x.CreatedByUserId.Equals(Guid.Parse(user!.Id)));
+    if (cart == null) {
+        cart = new Cart {
+            CartItems = [],
+            CreatedAt = DateTime.Parse(DateTime.UtcNow.ToString()),
+            CreatedByUserId = Guid.Parse(user!.Id),
+            Id = Guid.NewGuid(),
+        };
+        db.Carts.Add(cart);
+        db.SaveChanges();
+    }
+
+    var cartItem = db.CartItems.FirstOrDefault(x => x.CartId == cart.Id && x.Id.ToString() == itemId);
+    if (cartItem == null) {
+        return Results.BadRequest($"Item {itemId} not found in cart");
+    }
+
+    CrmProduct? product;
+    try {
+        product = await hc.GetFromJsonAsync<CrmProduct>($"http://crm.skills.lan/api/products/{cartItem.ProductId}");
+    } catch {
+        return Results.BadRequest($"Product {cartItem.ProductId} has insufficient quantity");
+    }
+
+    if (product?.AvailableQuantity < newQuantity) return Results.BadRequest($"Product {cartItem.ProductId} has insufficient quantity");
+
+    cartItem.Quantity = newQuantity ?? 0;
+    db.SaveChanges();
+
+    List<CartModelItem> completeItems = [];
+
+    foreach (var item in cart.CartItems) {
+        var productDetails = await hc.GetFromJsonAsync<CrmProduct>($"http://crm.skills.lan/api/products/{item.ProductId}");
+        completeItems.Add(new CartModelItem(item.Id.ToString(), productDetails?.Name ?? "", item.Quantity, productDetails?.Price ?? 0, (productDetails?.Price ?? 0) * item.Quantity));
+    }
+
+    return Results.Ok(new CartModel(cart.Id.ToString(), cart.CreatedAt.ToString("s").Split(".").First(), completeItems.Sum(x => x.TotalPrice), [.. completeItems]));
+});
+
 app.Run();
